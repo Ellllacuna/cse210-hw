f5218ed09ffe96380efc62e0856323627231b695
Ellllacuna <vunderbrod@gmail.com>
2025-04-25 17:13:48 -0600

    completed the stretch challenge

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        
+        int number = 1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do{
+            Console.Write("Enter number: ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }while (number != 0);
+
+        float sum = 0;
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+
+        int largestNum = -999999999;
+        foreach (int i in numbers)
+        {
+            if (i > largestNum)
+            {
+                largestNum = i;
+            }
+        }
+
+        int smallestNum = 99999999;
+        foreach (int i in numbers)
+        {
+            if (i < smallestNum && i > 0)
+            smallestNum = i;
+        }
+
+        float average = sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+        Console.WriteLine($"The smallest positive number is: {smallestNum}");
+        
+        Console.WriteLine("The sorted list is");
+        numbers.Sort();
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
