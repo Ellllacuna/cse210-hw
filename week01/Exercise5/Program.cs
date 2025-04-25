33da874846053f00ca44a77d8f222d0278cd14c0
Ellllacuna <vunderbrod@gmail.com>
2025-04-25 17:35:35 -0600

    completed the stretch challenge

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Enter favorite number: ");
+            string favNumberInput = Console.ReadLine();
+            int favNumber = int.Parse(favNumberInput);
+
+            return favNumber;
+        }
+
+        static int SquareNumber(int x)
+        {
+            int square = x * x;
+
+            return square;
+        }
+
+        static void DisplayResult(int favNumber, string name)
+        {
+            int square = SquareNumber(favNumber);
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        DisplayResult(number, name);
     }
 }
