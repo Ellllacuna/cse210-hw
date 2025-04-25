b6842dd378b0febfe384150ee2d4f55f776dd436
Ellllacuna <vunderbrod@gmail.com>
2025-04-25 10:04:01 -0600

    stretch challenge completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentageInput = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageInput);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            //Console.WriteLine("Grade: A");
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            //Console.WriteLine("Grade: B");
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+           //Console.WriteLine("Grade: C");
+           letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            //Console.WriteLine("Grade: C");
+            letter = "D";
+        }
+        else
+        {
+            //Console.WriteLine("Grade: F");
+            letter = "F";
+        }
+
+        int lastDigit = gradePercentage % 10;
+        string extra = "";
+        if (lastDigit >= 7 && !(gradePercentage >= 90))
+        {
+            extra = "+";
+        }
+        else if (lastDigit < 3 && !(gradePercentage < 60))
+        {
+            extra = "-";
+        }
+        else
+        {
+            extra = "";
+        }
+
+        Console.WriteLine($"Grade: {letter}{extra}");
+
+
+        if (gradePercentage >= 60)
+        {
+            Console.WriteLine("You passed");
+        }
+        else
+        {
+            Console.WriteLine("You Failed");
+        }
     }
 }
