30c3a0b1e870dbea8669c003dd62f3ca79463364
Ellllacuna <vunderbrod@gmail.com>
2025-04-25 09:14:23 -0600

    completed exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is you last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
 }
