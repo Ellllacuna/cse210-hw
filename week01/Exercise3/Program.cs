1d1b9eefa7337339949973dfd677efe7de5654a6
Ellllacuna <vunderbrod@gmail.com>
2025-04-25 10:33:03 -0600

    completed stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number? ");
+        //string magicNumber = Console.ReadLine();
+        //int number = int.Parse(magicNumber);
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,100);
+
+            string correct = "no";
+            int counter = 0;
+
+            do{
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                int guess = int.Parse(guessInput);
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                    correct = "yes";
+                    counter += 1;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    correct = "no";
+                    counter += 1;
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    correct = "no";
+                    counter += 1;
+                }
+            } while (correct != "yes");
+
+            Console.WriteLine($"Guesses: {counter}");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            string playAgainInput = Console.ReadLine();
+
+            if (playAgainInput != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thank you for playing!");
+            }
+        }
     }
 }
