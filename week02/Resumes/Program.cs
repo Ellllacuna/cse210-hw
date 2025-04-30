ca17f23b78723f1d9b55c05ba0fa3bab678b7308
Ellllacuna <vunderbrod@gmail.com>
2025-04-29 18:03:15 -0600

    resumes completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Zookeeper";
+        job2._company = "Norfolk Zoo";
+        job2._startYear = 2000;
+        job2._endYear = 2025;
+
+        //job1.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Larbold";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Console.WriteLine(myResume._jobs[0]._jobTitle);
+        myResume.DisplayResume();
+
     }
 }
