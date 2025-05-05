2b27de11a03ec69dc1a8f26c96213d7430433bd8
Ellllacuna <vunderbrod@gmail.com>
2025-05-05 15:45:26 -0600

    completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -3,7 +3,9 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+    {       
+        Fraction f1 = new Fraction( 1,  3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
