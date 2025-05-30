66345318ceb2a09b1ca9e9b55b489647c64ab507
Ellllacuna <vunderbrod@gmail.com>
2025-05-29 20:29:39 -0600

    list goal names

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -12,7 +12,6 @@ class Program
         do
         {
             Console.WriteLine("You have 0 points\n\n");
-            //update this with the actual points variable
             Console.WriteLine("Menu Options:\n    1. Create New Goal\n    2. List Goals\n    3. Save Goals\n    4. Load Goals\n    5. Record Event\n    6. Quit");
             Console.Write("Select a choice from the menu: ");
             string userInput = Console.ReadLine();
--- week06/EternalQuest/goalmanager.cs
+++ week06/EternalQuest/goalmanager.cs
@@ -15,7 +15,25 @@ public class GoalManager
         bool Quit = false;
         do
         {
-            
-        }while (!Quit);
+            Console.WriteLine("Menu Options:\n    1. Create New Goal\n    2. List Goals\n    3. Save Goals\n    4. Load Goals\n    5. Record Event\n    6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string userInput = Console.ReadLine();
+        } while (!Quit);
+    }
+
+    public void DisplayPlayerInfo()
+    {
+        Console.WriteLine($"\nYou have {_score} points\n");
+    }
+
+    public void ListGoalNames()
+    {
+        Console.WriteLine("The goals are:");
+        foreach (Goal goal in _goals)
+        {
+            int listNumber = _goals.IndexOf(goal) + 1;
+            Console.WriteLine($"{listNumber}. [ ] {goal.GetName()} ({goal.GetDesc()})");
+        }
+        Console.WriteLine();
     }
 }
