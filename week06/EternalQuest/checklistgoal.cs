f8d17c00fbb13c8273ce369476ae3d69842f364c
Ellllacuna <vunderbrod@gmail.com>
2025-05-29 20:57:30 -0600

    checklist done probs

--- week06/EternalQuest/checklistgoal.cs
+++ week06/EternalQuest/checklistgoal.cs
@@ -8,7 +8,45 @@ public class ChecklistGoal : Goal
 
     public ChecklistGoal(string name, string description, string points, int target, int bonus) : base(name, description, points)
     {
+        _amountCompleted = 0;
         _target = target;
         _bonus = bonus;
+    }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+        int totalPoints = int.Parse(_points);
+        if (_amountCompleted == _target)
+        {
+            totalPoints += _bonus;
+            Console.WriteLine($"Congratulations! You earned {_points} points and a bonus of {_bonus} points!");
+        }
+        else
+        {
+            Console.WriteLine($"Congratulations! You earned {_points} points!");
+        }
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailString()
+    {
+        if (IsComplete())
+        {
+            return $"[X] {_shortName} ({_description}) -- Currently completed: {_amountCompleted}/{_target}";
+        }
+        else
+        {
+            return $"[ ] {_shortName} ({_description}) -- Currently completed: {_amountCompleted}/{_target}";
+        }
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal: {_shortName}|{_description}|{_points}|{_bonus}|{_target}{_amountCompleted}";   
     }
 }
