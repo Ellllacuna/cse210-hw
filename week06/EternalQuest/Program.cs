5d05a58e48f409b0f955cb25f4bccd31a8998f6a
Ellllacuna <vunderbrod@gmail.com>
2025-05-29 18:48:02 -0600

    starting out

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        bool Quit = false;
+
+        do
+        {
+            Console.WriteLine("You have 0 points\n\n");
+            //update this with the actual points variable
+            Console.WriteLine("Menu Options:\n    1. Create New Goal\n    2. List Goals\n    3. Save Goals\n    4. Load Goals\n    5. Record Event\n    6. Quit");
+            
+        } while (!Quit);
     }
 }
---
+++ week06/EternalQuest/goal.cs
@@ -0,0 +1,23 @@
+using System;
+
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    public abstract void IsComplete();
+    public virtual string GetDetailsString()
+    {
+        return "string";
+    }
+    public abstract string GetStringRepresentation();
+}
